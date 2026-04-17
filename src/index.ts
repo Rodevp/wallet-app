17406a91877ed855b7941da87555931394cb64f2
Rodevp <rodriguezrodrigor12@gmail.com>
2026-04-17 08:41:48 -0500

    colocando algunas reglas de seguridad para hacerlo más robusto

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import express from 'express';
+import helmet from 'helmet';
+import { doubleCsrf } from 'csrf-csrf'
+
 import type { Response, Request } from 'express';
-import AppDataSource from './db';
 import { errorHandler } from './middlewares/errors';
+
+import AppDataSource from './db';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const { doubleCsrfProtection } = doubleCsrf({
+    getSecret: () => 'secret',
+    cookieName: 'x-csrf-token',
+    cookieOptions: {
+        httpOnly: true,
+        sameSite: 'strict',
+        secure: true,
+    },
+    getSessionIdentifier: (req: Request) => req.ip!,
+});
 
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+app.use(helmet());
+app.use(doubleCsrfProtection);
 
 app.get('/', (req: Request, res: Response) => {
     return res.status(200).json({ message: 'server on' });
